3518730206c72e027df739ffdbc5fd2997fe2182
Mansoor Bahramand <mansoor.bahramand@gMB96db97.local>
2018-09-24 20:13:17 -0700

    added a form submit handler and a pokemonId Input field

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+function getPokemon(id=1) {
 //Third party api request
-axios.get('https://pokeapi.co/api/v2/pokemon/3/')
+axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
 // ON success this is what is running
 .then(results => {
     //condense in the information into pokemon constant
@@ -38,3 +39,25 @@ axios.get('https://pokeapi.co/api/v2/pokemon/3/')
 .catch(err => {
   console.error("OH NO I HAVE FAILED, HERE IS WHY:", err.message);
 })
+}
+
+/**
+ * This handles the onsubmit on the form.
+ *
+ * @param {Object} event 
+ */
+function handleSubmit(event) {
+  // prevents form from submitting
+  event.preventDefault();
+  // grabs the pokemonId from the field
+  const pokemonId = document.getElementById('pokemonId').value;
+  // grabs the pokemon images list
+  const pokemonImages = document.getElementById('pokemonImages');
+  // clears out pokemon images by setting the innerHtml to nothign
+  pokemonImages.innerHTML = "";
+  // calls the getPokemon function with the new value
+  getPokemon(pokemonId);
+}
+//Initialize call
+getPokemon(6);
+
